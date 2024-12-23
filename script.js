34f81d111df6f7f048b93bc1febbb13e9f719ce5
Davizets123 <Davizets_andrade@hotmail.com>
2024-12-23 17:19:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function linksobre() {
+    window.location.href = "http://127.0.0.1:3000/sobre.html"
+}
+
+function veiculos() {
+    window.location.href = "http://127.0.0.1:3000/veiculos.html"
+}
+
+function contato() {
+    window.location.href = "https://web.whatsapp.com/5519998600011"
+}
+
+function paginicial() {
+    window.location.href = "http://127.0.0.1:3000/inicio.html"
+}
